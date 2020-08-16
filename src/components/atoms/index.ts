55f053dcdacf456f7b0db6a95ce3ce845ad33c23
tmitsuoka0423 <tmitsuoka0423@gmail.com>
2020-08-16 23:16:17 +0900

    add icon

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,6 +1,7 @@
 import Button from './Button';
 import Card from './Card';
 import Checkbox from './Checkbox';
+import Icon from './Icon';
 import Title from './Title';
 
-export { Button, Card, Checkbox, Title };
+export { Button, Card, Checkbox, Icon, Title };
