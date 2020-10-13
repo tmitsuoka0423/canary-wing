0548d70a31a8191c7007ab8ca5c6c66a652ebdc1
tmitsuoka0423 <tmitsuoka0423@gmail.com>
2020-10-13 22:14:24 +0900

    Revert "add icon"
    This reverts commit 55f053dcdacf456f7b0db6a95ce3ce845ad33c23.

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,7 +1,6 @@
 import Button from './Button';
 import Card from './Card';
 import Checkbox from './Checkbox';
-import Icon from './Icon';
 import Title from './Title';
 
-export { Button, Card, Checkbox, Icon, Title };
+export { Button, Card, Checkbox, Title };
